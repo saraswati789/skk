8fb9c9651e1a987fc88e4e949bf7989bfd0f8161
saraswati789 <37729424+saraswati789@users.noreply.github.com>
2018-03-28 17:56:28 +0100

    Update views.py

--- as/polls/views.py
+++ as/polls/views.py
@@ -4,10 +4,9 @@ db=MySQLdb.connect('localhost','root','','mnnit')
 cur=db.cursor()
 def index(request):
 	return render(request, 'polls/index.html');
-	cur.execute('insert into table1 values("nm","pas")');
 	if request.GET.get('q'):
 		nm=request.GET['nm']
 		pas=request.GET['pass']	
 		with db:
-			cur.execute('insert into table1 values(nm,pas)');
+			cur.execute("""insert into table1 values(%s,%s)""",(nm,pas)');
 		return render(request, 'polls/a.html');
